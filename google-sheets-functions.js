88becebf2b0a1c74740f8a78e48e4edb8d820eaa
Till Runge <till.runge@gmail.com>
2017-09-09 04:35:37 +0200

    spelling

--- google-sheets-functions.js
+++ google-sheets-functions.js
@@ -1,15 +1,15 @@
 /**
- * This file has several functions for crypto market tools which can be used with Google Sheets.
+ * This file has several functions as crypto market tools which can be used with Google Sheets.
  *
- * In Google Sheets simple click on 'Tools -> Script Editor...' in the menu and save the code there.
+ * In Google Sheets click on 'Tools -> Script Editor...' in the menu and save the code there.
  * To get the value of a function simply enter '=functionName(parameter1, parameter2)' in a cell.
- * For example: '=getTicker("GDAX", "last", "ETH-USD")'
+ * For example: '=getTicker("GDAX", "last", "ETH-USD")'.
  * If the Spreadsheet is in a locale which use a comma as decimal mark you, need to use a semicolon
- * as a parameter seperator. Like this: '=getTicker("GDAX"; "last"; "ETH-USD")'
+ * as a parameter seperator. Like this: '=getTicker("GDAX"; "last"; "ETH-USD")'.
  *
  * It is very helpful to put a cell with a current date as an extra parameter to the functions
  * like this: '=getTicker("GDAX", "last", "ETH-USD", A1)'. Every time the date is updated all the
- * functions will update themselves as well. A refresh button in the menu fo this is implemented in
+ * functions will update themselves as well. A refresh button in the menu for this is implemented with
  * the 'onOpen' and the 'refreshLastUpdate' functions.
  */
 
